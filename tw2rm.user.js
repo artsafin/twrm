34e1a6dbf2a39b8989711ced2c30f567b2a65321
Artur Safin <artur.safin@forextime.com>
2016-02-12 02:36:25 +0200

    Version 2 - better, higher, stronger

--- tw2rm.user.js
+++ tw2rm.user.js
@@ -4,11 +4,19 @@
 // @description TeamWox to Redmine
 // @version     2
 // @grant       GM_openInTab
+// @grant       GM_getResourceURL
 // @grant       unsafeWindow
 // @match https://tw.fxtm.com/tasks/view/*
 // @match https://tw.fxtm.com/servicedesk/view/*
 // @require     https://code.jquery.com/jquery-2.1.4.min.js
+// @resource redmineIcon https://redmine.fxtm/favicon.ico
 // ==/UserScript==
+
+var CFG = {
+    twUrl: "https://tw.fxtm.com",
+    redmineUrl: "https://redmine.fxtm",
+    redmineProject: "web-development-department"
+};
 
 this.$ = this.jQuery = jQuery.noConflict(true);
 
@@ -24,33 +32,90 @@ if (!String.prototype.format) {
   };
 }
 
-function intercept()
-{
-    var w = unsafeWindow.top;
+function createRedmineLink(){
+    var iconUrl = GM_getResourceURL('redmineIcon'),
+        tableRow = $('<td><a class="commands-panel-link with-image" '
+            + 'style="position: relative; z-index: 1001; background-image: url(\'{0}\'); background-size: 16px;" '.format(iconUrl)
+            + 'href="javascript:void(0)" '
+            + 'title="To redmine">'
+                + 'To redmine'
+            + '</a></td>');
 
-    var original = w.Controls.ContentViewExt.prototype.SetData;
-    w.Controls.ContentViewExt.prototype.SetData = 123;
-
-    console.log('1', w.Controls.ContentViewExt.prototype.SetData, w.Controls.ContentViewExt.SetData);
-/*
-    function(){
-        console.log('Before original', arguments);
-        original.apply(this, arguments);
-        console.log('After original');
-    }*/
+    return tableRow;
 }
 
+function createPanel() {
+    var style = 'z-index: 1000; position: absolute; right: 0; border: 0; border-radius: 5px; box-shadow: 0px 0px 20px 0 #dedbcd; display: none; width: 260px; height: 530px;';
+    console.log('createPanel');
+    var src = 
+            '<html>'
+            + '<head>'
+            + '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">'
+            + '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>'
+            + '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>'
+            + '</head>'
+            + '<body style="padding: 0.5em; background-color: #f8f7f3; margin-top: 2em;">'
++ '<form id="form">'
++ '<h5>Tracker</h5>'
++ '<div class="btn-group" data-toggle="buttons" role="group">'
+  + '<label class="btn btn-default active">'
+  + '<input type="radio" name="tracker_id" value="4" autocomplete="off" checked>Task'
+  + '</label>'
+  + '<label class="btn btn-default">'
+    + '<input type="radio" name="tracker_id" value="1" autocomplete="off">Bug'
+  + '</label>'
++ '</div>'
++ '<hr>'
++ '<h5>Team</h5>'
++ '<div class="list-group">'
++ '  <button type="submit" name="assign_to" value="0" class="list-group-item">None</button>'
++ '  <button type="submit" name="assign_to" value="134" class="list-group-item">Backoffice Team</button>'
++ '  <button type="submit" name="assign_to" value="140" class="list-group-item">Frontend Team</button>'
++ '  <button type="submit" name="assign_to" value="142" class="list-group-item">Drupal Team</button>'
++ '  <button type="submit" name="assign_to" value="137" class="list-group-item">Partnership Team</button>'
++ '  <button type="submit" name="assign_to" value="139" class="list-group-item">CRM Team</button>'
++ '  <button type="submit" name="assign_to" value="138" class="list-group-item">Dealing Team</button>'
++ '  <button type="submit" name="assign_to" value="141" class="list-group-item">Investments Team</button>'
++ '</div>'
++ '</form>'
++ '<script>var f = $("#form"); f.find(":submit").click(function(e){e.preventDefault(); var d = {"issue[tracker_id]": f.find("[name=tracker_id]:checked").val(), "issue[assigned_to_id]": e.target.value}; window.parent.postMessage(d, "*"); })'
++ '</script>'
+            + '</body></html>';
 
-function addRedmineLink(){
-    var tableRow = $('<td><a class="commands-panel-link with-image" style="background-image: url(\'https://redmine.fxtm/favicon.ico\');" href="javascript:void(0)" title="To redmine">To redmine</a></td>');
-    var container = $('.head .commands table tr');
-    // console.log(container, tableRow);
-    container.prepend(tableRow);
-    return tableRow.find('a');
+    return $('<iframe style=\'{0}\' srcdoc=\'{1}\'></iframe>'.format(style, src));
 }
 
 function getTwHost() {
     return window.location.protocol + "//" + window.location.host;
+}
+
+function getSelectionContext() {
+    var selection = window.getSelection();
+    if (selection.rangeCount <= 0) {
+        return null;
+    }
+
+    var range = selection.getRangeAt(0);
+
+    if (range.startOffset == range.endOffset) {
+        return null;
+    }
+
+    console.log('range', range);
+
+    var startEl = $(range.startContainer);
+    var commentItemEl = startEl.closest('div.item');
+    var selectionLink = commentItemEl.length ? commentItemEl.find('.head a.link') : startEl.find('div.item').prev('div.item').find('.head a.link');
+
+    var clonedSelection = range.cloneContents();
+    var div = document.createElement('div');
+    div.appendChild(clonedSelection);
+
+    return {
+        linkName: selectionLink.text(),
+        href: selectionLink.attr('href'),
+        html: div.innerHTML
+    };
 }
 
 function getTwPostData() {
@@ -59,6 +124,7 @@ function getTwPostData() {
         title: $('.content-view-ext div.head div.title div.text').html(),
         self_link: getTwHost() + $('div.head div.details a.link').attr('href'),
         head_content: $('.content-view-ext div.body div.content').html(),
+        sel: getSelectionContext() || {}
     };
 }
 
@@ -75,21 +141,14 @@ RedmineLink.prototype = {
         this.fields[f] = v;
     },
     render: function() {
-        var link = "{0}/projects/{1}/issues/new?".format(this.baseUrl, this.projectAlias);
-        for (var f in this.fields) if (this.fields.hasOwnProperty(f)) {
-            link += "&issue[{0}]={1}".format(f, encodeURIComponent(this.fields[f]));
-        }
+        var link = "{0}/projects/{1}/issues/new?".format(this.baseUrl, this.projectAlias)
+            + $.param(this.fields);
 
         return link;
     }
 };
 
 function htmlToTextile(html){
-    // <img title="" src="/servicedesk/image/548690" style="vertical-align:middle;" height="995" width="2244">
-
-    // 
-    // console.log('html img', html);
-
     try {
         html = html.replace(/(<br>|<\/li>)/g, "\n");
         html = html.replace(/<\/p>/g, "\n\n");
@@ -97,10 +156,8 @@ function htmlToTextile(html){
         html = html.replace(/<p>(.)/g, "p. $1");
         html = html.replace(/(<b>|<\/b>)/g, "*");
         html = html.replace(/(<ul>|<\/ul>)/g, "");
-        html = html.replace(/<img.*?src="([^"]+)".*?>/g, " !{max-width: 100%}{0}$1! ".format(getTwHost()));
+        html = html.replace(/<img.*?src="([^"]+)".*?>/g, " !{0}$1! ".format(getTwHost()));
         html = html.replace(/<.*?>/g, "");
-
-        // console.log('html', html);
 
         return html;
 
@@ -114,35 +171,72 @@ function htmlToTextile(html){
     return ":(";
 }
 
-var redminizedLink = null;
+var panelToggler = null;
 
 function redminize() {
-    redminizedLink = addRedmineLink();
+    var container = $('.head .commands table tr'),
+        redminizedLinkCell = createRedmineLink(),
+        redminizedLink = redminizedLinkCell.find('a');
 
-    redminizedLink.on('click', function(){
-        var tw = getTwPostData(),
-            rm = new RedmineLink('https://redmine.fxtm', 'web-development-department');
+    container.prepend(redminizedLinkCell);
 
-        rm.setFields({
-            subject: tw.title,
-            tracker_id: 4, // Task
-            description: "TW: {0}\n\n{1}".format(tw.self_link, htmlToTextile(tw.head_content).replace(/^/mg, '> '))
+    var panel = createPanel();
+    redminizedLinkCell.append(panel);
+
+    panelToggler = function(){
+        panel.slideToggle(100);
+    };
+
+    redminizedLink
+        .on('click.twrm', panelToggler)
+        .one("remove", function () {
+            console.log('cleanup - link');
+            redminizedLink.off('.twrm');
         });
-        // https://redmine.fxtm/projects/unixdp/issues/new?issue[subject]=Query&issue[assigned_to_id]=6&issue[tracker_id]=7&issue[description]=%3Cpre%3E%0D%0A%0D%0A%0D%0A%3C/pre%3E
-        var linkText = rm.render();
 
-        // console.log(linkText);
-        GM_openInTab(linkText, false);
-    });
-
-    $('div.page_numerator a').on('click', function(){
-        // console.log('a!');
-        setTimeout(redminize, 1000);
-    });
+    $('div.page_numerator a')
+        .on('click.twrm', function(){
+            setTimeout(redminize, 1000);
+        })
+        .one('remove', function(){
+            console.log('cleanup - a');
+            $(this).off('.twrm');
+        });
 }
+
+function openRedmineWithParams(tw, overrideParams) {
+    var rm = new RedmineLink(CFG.redmineUrl, CFG.redmineProject);
+
+    var description = htmlToTextile(tw.sel.html || tw.head_content).replace(/^/mg, '> ');
+    var twLink = tw.sel.href ? "TW post @{0}@: {1}".format(tw.sel.linkName, tw.sel.href) : "TW: {0}".format(tw.self_link);
+
+    rm.setFields($.extend({
+        "issue[subject]": tw.title,
+        "issue[tracker_id]": 4, // Task
+        "issue[description]": twLink + "\n\n" + description,
+        "issue[custom_field_values]": {
+            "1": tw.sel.href || tw.self_link
+        }
+    }, overrideParams || {}));
+    var linkText = rm.render();
+    
+    GM_openInTab(linkText, false);
+}
+
+window.addEventListener("message", function(event) {
+    var origin = event.origin || event.originalEvent.origin;
+    if (origin != CFG.twUrl) {
+        return;
+    }
+
+    var tw = getTwPostData();
+    openRedmineWithParams(tw, event.data);
+
+    panelToggler();
+}, false);
 
 setTimeout(function(){
     redminize();
 }, 1000);
 
-console.log('tw2rm');
+console.log('tw2rm v.2');
